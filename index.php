4b7d3de3ef3c118ad42b862f3b350f0495557bf6
a1083358 <100353517+a1083358@users.noreply.github.com>
2022-03-23 20:05:04 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+        <meta name="keywords" content="登入, 墾丁三日遊">
+        <title>登入</title>
+        <link rel="icon" href="/img/icon.jpg" type="image/x-con"/>
+</head>
+<body>
+<center><h2>墾丁三日遊</h2></center>    
+<center><h1>登入</h1></center>
+    <center><form action="record.php" method="get" style="width: 80%;" enctype="multipart/form-data">
+        帳號：<input type="text" name="euserid" placeholder="userid" required><br><br>
+        密碼：<input type="password" name="epassword" placeholder="password" required><br><br>
+        <input type="submit"><br><br>
+    </form></center>
+    <p>帳:cindy83392</p>
+    <p>密:123</p>
+</body>
+</html>
